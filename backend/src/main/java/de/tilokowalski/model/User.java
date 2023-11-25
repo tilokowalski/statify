18fed80ee2bd586cf9822e451c244763f364fca5
Höhl, Lukas <lukas.hoehl@accso.de>
2023-11-25 15:56:46 +0100

    feat(backend): surrealdb storage
    Signed-off-by: Höhl, Lukas <lukas.hoehl@accso.de>

--- backend/src/main/java/de/tilokowalski/model/User.java
+++ backend/src/main/java/de/tilokowalski/model/User.java
@@ -1,9 +1,24 @@
 package de.tilokowalski.model;
 
+import de.tilokowalski.db.Record;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-public class User {
+@Getter
+@Setter
+public class User extends Record {
 
   String name;
+
+  public User(String name) {
+    this("", name);
+  }
+
+
+  public User(String recordId, String name) {
+    super("user", recordId);
+    this.name = name;
+  }
 }
